6e732382b6ac1f91ab72732ed93db4d243339976
Keith Morris <keith.morris@moxieusa.com>
2015-02-24 13:33:41 -0500

    update newsite.js script to use externalized config variables and async.series to orchestrate the steps of site creation.

---
+++ config/default.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Keith Morris on 2/24/15.
+ */
+module.exports = {
+    newsite: {
+        defaults: {
+            parent: '/Users/kmorris/Sites',
+            webroot: '',
+            index: false
+        },
+        vhostConfDir: '/etc/apache2/httpd.vhosts/',
+        vhostTemplateFile: 'vhostTemplate.conf',
+        reverseDomainFormat: true
+    }
+};
---
+++ newsite.js
@@ -0,0 +1,98 @@
+#!/usr/bin/env node
+"use strict";
+var config = require('config').get('newsite'),
+    argv = require('minimist')(process.argv.slice(2), {
+        alias: {
+            parent: 'p',
+            webroot: 'w',
+            index: 'i'
+        },
+        default: config.get('defaults')
+    }),
+    async = require('async'),
+    exec = require('child_process').exec,
+    fs = require('node-fs'),
+    path = require('path');
+
+if (!argv._.length) {
+    console.log([
+        "  \nUsage: newsite www.localtest.com [--webroot 'public' --parent '/var/www' --index] \n",
+        "  dot-separated domain name (e.g. www.example.com) is the only required parameter\n",
+        "  Optional Flags:",
+        "    -w, --webroot [value]    The subdirectory to use as webroot. Defaults to empty (The same as app directory)",
+        "    -p, --parent [value]     The directory that will be the parent of app directory (e.g. /Users/name/Sites)",
+        "    -i, --index              If set, this will generate a simple index.php file with phpinfo();\n"
+    ].join("\n"));
+    process.exit();
+}
+
+var parentDir = argv.parent,
+    domain = argv._[0],
+    directoryName = (config.get('reverseDomainFormat')) ? domain.split('.').reverse().join('.') : domain,
+    webroot = argv.webroot,
+    siteDir = path.join(parentDir, directoryName, webroot),
+    vhostConfDir = config.get('vhostConfDir'),
+    createIndex = argv.index,
+    templateConf = config.get('vhostTemplateFile');
+
+async.series([
+    createVhostFile,
+    createSiteDirectory,
+    restartApache
+], function (err, results) {
+    console.log('New site configuration completed.');
+});
+
+// ********************************************************************
+// Functions
+// ********************************************************************
+function createVhostFile(done) {
+    console.log('Creating vhost configuration file.');
+    fs.readFile(templateConf, 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = data.replace(/%%SITE_DIR%%/g, parentDir);
+        data = data.replace(/%%DOMAIN%%/g, domain);
+        data = data.replace(/%%DOMAIN%%/g, domain);
+        data = data.replace(/%%WEBROOT%%/g, webroot);
+        data = data.replace(/%%DIRECTORY%%/g, directoryName);
+
+        fs.writeFile(path.join(vhostConfDir, directoryName + '.conf'), data, 'utf8', function (err) {
+            if (err) {
+                console.log('Error Writing Config file.');
+            }
+            done(err);
+        });
+    });
+}
+
+function createSiteDirectory(done) {
+    console.log('Creating Site Directory at ' + siteDir);
+    fs.mkdir(siteDir, '0777', true, function (err) {
+        if (err) {
+            console.log('Error creating directory:', err);
+            return done(err);
+        }
+        if (createIndex) {
+            fs.writeFile(path.join(siteDir, 'index.php'), '<?php phpinfo();', function (err) {
+                if (err) {
+                    console.log('error writing info file');
+                }
+                done(err);
+            });
+        } else {
+            done();
+        }
+    });
+}
+
+function restartApache(done) {
+    console.log('Restarting Apache.');
+    var cp = exec('sudo apachectl restart', function (err) {
+        if (err) {
+            console.log('error restarting apache');
+        }
+        done(err);
+    });
+}
