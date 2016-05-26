c9f369aa7528903c0ebc811eef41d314dc46e870
Keith Morris <keith.morris@moxieusa.com>
2016-05-26 07:46:54 -0400

    Add creds.sample.js file

---
+++ creds.sample.js
@@ -0,0 +1,14 @@
+/**
+ * Created by kmorris on 11/8/14.
+ */
+ /* jshint node:true */
+
+ // *********************************************************
+ // copy this file to creds.js and update the UN and PW below
+ // *********************************************************
+ 
+"use strict";
+module.exports = {
+	username: "kmorris",
+	password: "passwordHere!"
+};
